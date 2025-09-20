e539d0f606b8728bb29612119df4756014d5a093
Elina <elina.dobruckaya2005@gmail.com>
2025-09-20 15:47:08 +0300

    Slider section updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello world!')
+import $ from 'jquery'
+import 'slick-carousel'
+
+$(document).ready(function () {
+  $('.slider-container').slick({
+    // важно — именно контейнер, где лежат слайды
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+  })
+})
